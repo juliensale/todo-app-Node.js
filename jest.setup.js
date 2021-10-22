f92719b2a29c8050b8ad484336bc11d62e08dbb8
saltyozux <75158693+saltyozux@users.noreply.github.com>
2021-10-22 08:58:48 +0200

    Fixed test database management

--- jest.setup.js
+++ jest.setup.js
@@ -1,16 +1,45 @@
-const { getTestDatabase, removeTestDatabase } = require("./testingFunctions");
+const { getTestDatabase, removeTestDatabase, wait } = require("./testingFunctions");
 
-const wait = (time) => new Promise((res,) => {
-	setTimeout(() => { res() }, time);
+
+// beforeAll(async () => {
+// 	// Wait for the previous database to be deleted
+// 	await wait(500).then(() => { })
+// 	global.sequelize = getTestDatabase();
+// })
+
+const waitForDBDeleted = () => new Promise(async (res, rej) => {
+	// Retries every 100ms for 5 seconds
+	for (let i = 0; i < 500; i++) {
+		// if global.isDBClean switches to `true`, resolves
+		await wait(100).then(() => {
+			if (global.isDBClean) {
+				res()
+			}
+		})
+	}
+	// if global.isDBClean does not switch during these 5 seconds, rejects
+	rej()
 })
 
 beforeAll(async () => {
+	// Sets a default value for the isDBClean variable
+	if (global.isDBClean === undefined) {
+		global.isDBClean = true
+	}
+
 	// Wait for the previous database to be deleted
-	await wait(500).then(() => { })
-	global.sequelize = getTestDatabase();
+	await waitForDBDeleted()
+		.then(() => {
+			// Initialises the DB
+			global.sequelize = getTestDatabase();
+			global.sequelize.query('PRAGMA journal_mode = "OFF"')
+
+			// Resets the isDBClean variable
+			global.isDBClean = false
+		})
 })
 
 
-afterAll(() => {
-	return removeTestDatabase(global.sequelize);
+afterAll(async () => {
+	await removeTestDatabase(global.sequelize);
 })
--- testingFunctions.js
+++ testingFunctions.js
@@ -6,15 +6,38 @@ const createTaskModel = require('./models/createModel/createTask');
 const createSubtaskModel = require('./models/createModel/createSubtask');
 const { unlinkSync } = require('fs');
 
+const wait = (time) => new Promise((res,) => {
+	setTimeout(() => { res() }, time);
+})
 const getTestDatabase = () => new Sequelize({ dialect: 'sqlite', storage: './db-test.sqlite', logging: false });
 
-const removeTestDatabase = (sequelize) => {
+const deleteTestDatabase = () => new Promise(async (res, rej) => {
+	let error;
+	for (let i = 0; i < 50; i++) {
+
+		// Wait for the connection to close before deleting the file
+		await wait(50)
+			.then(() => {
+				try {
+					unlinkSync('./db-test.sqlite')
+					res()
+				} catch (err) {
+					if (err.code == 'ENOENT') {
+						res()
+					}
+					error = err
+				}
+			})
+	}
+	rej(error)
+})
+
+const removeTestDatabase = async (sequelize) => {
 	let queryInterface = sequelize.getQueryInterface();
 	queryInterface.sequelize.connectionManager.connections.default.close() // manually close the sqlite connection which sequelize.close() omits
-	try {
-		// Wait for the connection to close before deleting the file
-		setTimeout(() => unlinkSync('./db-test.sqlite'), 300);
-	} catch (error) { }
+	await deleteTestDatabase()
+		.then(() => { global.isDBClean = true })
+		.catch(err => { throw err })
 };
 
 const createModels = async (sequelize, DataTypes, models) => {
@@ -48,4 +71,4 @@ const removeInstances = async (instances, models) => {
 	}
 }
 
-module.exports = { getTestDatabase, removeTestDatabase, createModels, removeInstances };
+module.exports = { getTestDatabase, removeTestDatabase, createModels, removeInstances, wait };
