b09bdbea923dad04d86a67e5296d8c8b3d39bbf3
saltyozux <75158693+saltyozux@users.noreply.github.com>
2021-10-23 10:12:59 +0200

    List get endpoint tests

---
+++ controllers/createController/createListController.js
@@ -0,0 +1,158 @@
+
+const createAuthentication = require('../../middleware/createAuthentication');
+
+const createListController = (User, List) => {
+
+	const { isAuthenticated } = createAuthentication(User)
+
+	const list_get = async (req, res) => {
+		const [err, user] = await isAuthenticated(req, res);
+		if (!err) {
+			List.findAll({ where: { UserId: user.id } })
+				.then(lists => {
+					res.status(200).send(lists);
+				})
+				.catch(err => {
+					console.log(err);
+					res.status(500).send('Server error.')
+				});
+		}
+	};
+
+	const list_details_get = async (req, res) => {
+		const [err, user] = await isAuthenticated(req, res);
+		if (!err) {
+			List.findOne({ where: { id: req.params.id, UserId: user.id } })
+				.then(list => {
+					if (!list) {
+						return res.status(404).send('No list found.');
+					}
+					res.status(200).send(list);
+				})
+				.catch(err => {
+					console.log(err);
+					res.status(500).send('Server error.')
+				})
+		}
+	};
+
+	const list_create = async (req, res) => {
+		const [err, user] = await isAuthenticated(req, res);
+		if (!err) {
+			try {
+				const { title, color } = req.body;
+
+				// Checking types
+				if (!(
+					typeof (title) === 'string' && title
+					&& (color ? typeof (color) === 'string' : true)
+				)) {
+					return res.status(400).send('Invalid credentials.');
+				}
+
+				var payload = {
+					title: title,
+					UserId: user.id
+				}
+
+				if (color) {
+					payload.color = color
+				}
+
+				// Creating the instance
+				List.create(payload)
+					.then(list => { res.status(201).send(list) })
+					.catch(err => {
+						console.log(err);
+						res.status(400).send('Invalid credentials.');
+					});
+			}
+			catch {
+				// If req.body could not be deconstruced
+				res.status(400).send('Invalid credentials.')
+			}
+		}
+	};
+
+	const list_update = async (req, res) => {
+		const [err, user] = await isAuthenticated(req, res);
+		if (!err) {
+			try {
+				const { title, color } = req.body;
+
+				// Checking types
+				if (!(
+					(title ? typeof (title) === 'string' : true)
+					&& (color ? (typeof (color) === 'string') : true)
+				)) {
+					return res.status(400).send('Invalid credentials.')
+				}
+
+				// Finding the list
+				List.findOne({ where: { id: req.params.id, UserId: user.id } })
+					.then(async (list) => {
+						// Missing list
+						if (!list) {
+							return res.status(404).send('List not found.');
+						}
+
+						// Updating it
+						if (title) {
+							list.title = title;
+						}
+						if (color) {
+							list.color = color;
+						}
+						return await list.save()
+							.then(() => { return res.status(200).send(list) })
+							.catch(err => {
+								console.log(err);
+								return res.status(500).send('Server error.')
+							})
+					})
+					.catch(err => {
+						console.log(err);
+						return res.status(500).send('Server error.')
+					})
+
+			}
+			catch {
+				// If req.body could not be deconstruced
+				res.status(400).send('Invalid credentials.')
+			}
+
+		}
+	};
+
+	const list_delete = async (req, res) => {
+		const [err, user] = await isAuthenticated(req, res);
+		if (!err) {
+			// Finding the list
+			List.findOne({ where: { id: req.params.id, UserId: user.id } })
+				.then(async (list) => {
+					if (!list) {
+						return res.status(404).send('No list found.');
+					}
+
+					// Deleting
+					return await list.destroy()
+						.then(() => {
+							return res.status(205).send('List deleted.');
+						})
+						.catch(err => {
+							console.log(err);
+							return res.status(500).send('Server error.')
+						})
+				})
+				.catch(err => {
+					console.log(err);
+					return res.status(500).send('Server error.')
+				})
+		}
+	}
+
+	return { list_get, list_details_get, list_create, list_update, list_delete };
+
+}
+
+module.exports = createListController;
---
+++ controllers/tests/listController.test.js
@@ -0,0 +1,106 @@
+'use strict'
+
+const { DataTypes } = require('sequelize');
+const { createModels, removeInstances } = require('../../testingFunctions');
+const createListController = require('../createController/createListController');
+const request = require('supertest');
+const express = require('express');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+
+describe("Tests the user controller", () => {
+	let sequelize;
+	let models = {
+		User: undefined,
+		List: undefined,
+		Sublist: undefined,
+		Task: undefined,
+		Subtask: undefined,
+	};
+	let instances = {
+		user: undefined,
+		list1: undefined,
+		list2: undefined,
+		userControl: undefined,
+		listControl: undefined
+	};
+
+	let username = 'TestUser';
+	let authToken = jwt.sign({ username: username }, process.env.TOKEN_KEY);
+
+	const app = express();
+	app.use(express.json());
+
+	beforeAll(async () => {
+		sequelize = global.sequelize;
+		return await sequelize.authenticate()
+			.then(async () => {
+				return await createModels(sequelize, DataTypes, models)
+					.then(() => {
+						const listController = createListController(models.User, models.List);
+						app.get('/list', listController.list_get);
+					})
+					.catch(err => { throw err });
+			})
+			.catch(err => { throw err });
+	});
+
+	beforeEach(async () => {
+		instances.user = await models.User.create({
+			username: username,
+			password: 'testpass123'
+		}).catch(err => { throw err });
+
+		instances.list1 = await models.List.create({
+			title: "Test list 1",
+			color: "#45efa8",
+			UserId: instances.user.id
+		}).catch(err => { throw err });
+
+		instances.list2 = await models.List.create({
+			title: "Test list 2",
+			color: "#65bfa3",
+			UserId: instances.user.id
+		}).catch(err => { throw err });
+
+		instances.userControl = await models.User.create({
+			username: 'UserControl',
+			password: 'testpass123'
+		}).catch(err => { throw err });
+
+		instances.listControl = await models.List.create({
+			title: "List control",
+			color: "#45efa8",
+			UserId: instances.userControl.id
+		}).catch(err => { throw err });
+
+	});
+
+	afterEach(async () => {
+		return await removeInstances(instances, models).catch(err => { throw err });
+	});
+
+	describe("Tests the 'list_get' controller", () => {
+		// We can limit to testing the absence of AuthenticationToken since every fail based on authentication is managed the same way
+		it("should fail because of the authentication", (done) => {
+			request(app)
+				.get('/list')
+				.expect('Authentication required. Set `AuthenticationToken` header with the authentication token.')
+				.expect(403, done);
+		});
+
+		it("should return the two lists but not the controll list", (done) => {
+			request(app)
+				.get('/list')
+				.set('AuthenticationToken', authToken)
+				.expect(result => {
+					expect(result.body.length).toBe(2)
+					expect(result.body[0].title).toBe(instances.list1.title)
+					expect(result.body[1].title).toBe(instances.list2.title)
+				})
+				.expect(200, done)
+		})
+	})
+
+});
