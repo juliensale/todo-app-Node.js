88e8927d7b1cfd24bc3c864ad40b79c239c4ab92
saltyozux <75158693+saltyozux@users.noreply.github.com>
2021-10-24 10:46:22 +0200

    Basic subtask endpoints tests (copied from task tests)

---
+++ controllers/tests/subtaskController.test.js
@@ -0,0 +1,387 @@
+'use strict'
+
+const { DataTypes } = require('sequelize');
+const { createModels, removeInstances } = require('../../testingFunctions');
+const createSubtaskController = require('../createController/createSubtaskController');
+const request = require('supertest');
+const express = require('express');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+
+describe("Tests the user controller", () => {
+	let sequelize;
+	let models = {
+		User: undefined,
+		List: undefined,
+		Sublist: undefined,
+		Task: undefined,
+		Subtask: undefined,
+	};
+	let instances = {
+		user: undefined,
+		list: undefined,
+		sublist: undefined,
+		task: undefined,
+		subtask1: undefined,
+		subtask2: undefined,
+		userControl: undefined,
+		listControl: undefined,
+		sublistControl: undefined,
+		taskControl: undefined,
+		subtaskControl: undefined
+	};
+
+	let username = 'TestUser';
+	let authToken = jwt.sign({ username: username }, process.env.TOKEN_KEY);
+
+	const app = express();
+	app.use(express.json());
+
+	beforeAll(async () => {
+		sequelize = global.sequelize;
+		return await sequelize.authenticate()
+			.then(async () => {
+				return await createModels(sequelize, DataTypes, models)
+					.then(() => {
+						const subtaskController = createSubtaskController(models.User, models.Subtask);
+						app.get('/subtask', subtaskController.subtask_get);
+						app.get('/subtask/:id', subtaskController.subtask_details_get);
+						app.post('/subtask', subtaskController.subtask_create);
+						app.patch('/subtask/:id', subtaskController.subtask_update);
+						app.delete('/subtask/:id', subtaskController.subtask_delete);
+					})
+					.catch(err => { throw err });
+			})
+			.catch(err => { throw err });
+	});
+
+	beforeEach(async () => {
+		instances.user = await models.User.create({
+			username: username,
+			password: 'testpass123'
+		}).catch(err => { throw err });
+
+		instances.list = await models.List.create({
+			title: "Test list",
+			UserId: instances.user.id
+		}).catch(err => { throw err });
+
+		instances.sublist = await models.Sublist.create({
+			title: "Test sublist",
+			UserId: instances.user.id,
+			ListId: instances.list.id
+		}).catch(err => { throw err });
+
+		instances.task = await models.Task.create({
+			title: "Test task",
+			UserId: instances.user.id,
+			SublistId: instances.sublist.id
+		}).catch(err => { throw err });
+
+		instances.subtask1 = await models.Subtask.create({
+			title: "Test subtask 1",
+			UserId: instances.user.id,
+			TaskId: instances.task.id
+		}).catch(err => { throw err });
+
+		instances.subtask2 = await models.Subtask.create({
+			title: "Test subtask 2",
+			UserId: instances.user.id,
+			TaskId: instances.task.id
+		}).catch(err => { throw err });
+
+		instances.userControl = await models.User.create({
+			username: 'UserControl',
+			password: 'testpass123'
+		}).catch(err => { throw err });
+
+		instances.listControl = await models.List.create({
+			title: "List control",
+			color: "#45efa8",
+			UserId: instances.userControl.id
+		}).catch(err => { throw err });
+
+		instances.sublistControl = await models.Sublist.create({
+			title: "Sublist control",
+			UserId: instances.userControl.id,
+			ListId: instances.listControl.id
+		}).catch(err => { throw err });
+
+		instances.taskControl = await models.Task.create({
+			title: "task control",
+			UserId: instances.userControl.id,
+			SublistId: instances.sublistControl.id
+		}).catch(err => { throw err });
+
+		instances.subtaskControl = await models.Subtask.create({
+			title: "subtask control",
+			UserId: instances.userControl.id,
+			TaskId: instances.taskControl.id
+		}).catch(err => { throw err });
+
+	});
+
+	afterEach(async () => {
+		return await removeInstances(instances, models).catch(err => { throw err });
+	});
+
+	describe("Tests the 'subtask_get' controller", () => {
+		// We can limit to testing the absence of AuthenticationToken since every fail based on authentication is managed the same way
+		it("should fail because of the authentication", (done) => {
+			request(app)
+				.get('/subtask')
+				.expect('Authentication required. Set `AuthenticationToken` header with the authentication token.')
+				.expect(403, done);
+		});
+
+		it("should return the two subtasks but not the control subtask", (done) => {
+			request(app)
+				.get('/subtask')
+				.set('AuthenticationToken', authToken)
+				.expect(result => {
+					expect(result.body.length).toBe(2)
+					expect(result.body[0].title).toBe(instances.subtask1.title)
+					expect(result.body[1].title).toBe(instances.subtask2.title)
+				})
+				.expect(200, done)
+		})
+	})
+
+	describe("Tests the `subtask_details_get' controller", () => {
+		// We can limit to testing the absence of AuthenticationToken since every fail based on authentication is managed the same way
+		it("should fail because of the authentication", (done) => {
+			request(app)
+				.get(`/subtask/${instances.subtask1.id}`)
+				.expect('Authentication required. Set `AuthenticationToken` header with the authentication token.')
+				.expect(403, done);
+		});
+
+		it("should fail correctly when the id is incorrect", (done) => {
+			request(app)
+				.get('/subtask/invalidid')
+				.set('AuthenticationToken', authToken)
+				.expect('No subtask found.')
+				.expect(404, done);
+		});
+
+		it("should not be able to find an existing subtask", (done) => {
+			request(app)
+				.get('/subtask/942349')
+				.set('AuthenticationToken', authToken)
+				.expect('No subtask found.')
+				.expect(404, done);
+		});
+
+		it("should retrieve the subtask", (done) => {
+			request(app)
+				.get(`/subtask/${instances.subtask1.id}`)
+				.set('AuthenticationToken', authToken)
+				.expect(result => {
+					expect(result.body.title).toBe(instances.subtask1.title);
+				})
+				.expect(200, done);
+		});
+
+	})
+
+	describe("Tests the 'subtask_create' controller", () => {
+		// We can limit to testing the absence of AuthenticationToken since every fail based on authentication is managed the same way
+		it("should fail because of the authentication", (done) => {
+			request(app)
+				.post("/subtask")
+				.send({
+					title: "ShouldNotExist",
+					TaskId: instances.task.id
+				})
+				.expect('Authentication required. Set `AuthenticationToken` header with the authentication token.')
+				.expect(async () => {
+					await models.Subtask.findOne({ where: { title: "ShouldNotExist" } })
+						.then(subtask => {
+							expect(subtask).toBe(null)
+						})
+						.catch(err => { throw err })
+				})
+				.expect(403, done);
+		});
+
+		it("should create a subtask linked to the sublist", (done) => {
+			request(app)
+				.post('/subtask')
+				.set('AuthenticationToken', authToken)
+				.send({
+					title: "Test subtask",
+					TaskId: instances.task.id
+				})
+				.expect(result => {
+					const subtask = result.body;
+					expect(subtask.title).toBe("Test subtask");
+					expect(subtask.UserId).toBe(instances.user.id);
+					expect(subtask.TaskId).toBe(instances.task.id);
+				})
+				.expect(201, done);
+		})
+
+		it("should not accept a non-string title", (done) => {
+			request(app)
+				.post('/subtask')
+				.set('AuthenticationToken', authToken)
+				.send({
+					title: 4,
+					TaskId: instances.task.id
+				})
+				.expect('Invalid credentials.')
+				.expect(400, done);
+		});
+
+		it("should not accept an invalid TaskId", (done) => {
+			request(app)
+				.post('/subtask')
+				.set('AuthenticationToken', authToken)
+				.send({
+					title: "Test task",
+					TaskId: 545648551
+				})
+				.expect('Invalid credentials.')
+				.expect(400, done);
+		});
+
+		it("should require the title", (done) => {
+			request(app)
+				.post('/subtask')
+				.set('AuthenticationToken', authToken)
+				.send({
+					TaskId: instances.task.id
+				})
+				.expect('Invalid credentials.')
+				.expect(400, done);
+		});
+
+		it("should require the TaskId", (done) => {
+			request(app)
+				.post('/subtask')
+				.set('AuthenticationToken', authToken)
+				.send({
+					title: "Test subtask"
+				})
+				.expect('Invalid credentials.')
+				.expect(400, done);
+		});
+
+		it("should fail correctly without a body", (done) => {
+			request(app)
+				.post('/subtask')
+				.set('AuthenticationToken', authToken)
+				.expect('Invalid credentials.')
+				.expect(400, done);
+		});
+
+	})
+
+	describe("Tests the 'subtask_update' controller", () => {
+		// We can limit to testing the absence of AuthenticationToken since every fail based on authentication is managed the same way
+		it("should fail because of the authentication", (done) => {
+			request(app)
+				.patch(`/subtask/${instances.subtask1.id}`)
+				.send({
+					title: "ShouldNotExist"
+				})
+				.expect('Authentication required. Set `AuthenticationToken` header with the authentication token.')
+				.expect(async () => {
+					await models.Subtask.findOne({ where: { title: "ShouldNotExist" } })
+						.then(subtask => {
+							expect(subtask).toBe(null)
+						})
+						.catch(err => { throw err })
+				})
+				.expect(403, done);
+		});
+
+		it("should not find a subtask", (done) => {
+			request(app)
+				.patch('/subtask/wrongid')
+				.set('AuthenticationToken', authToken)
+				.send({
+					title: "Test"
+				})
+				.expect('No subtask found.')
+				.expect(404, done);
+		});
+
+		it("should change the subtask title", (done) => {
+			request(app)
+				.patch(`/subtask/${instances.subtask1.id}`)
+				.set('AuthenticationToken', authToken)
+				.send({
+					title: "Test update"
+				})
+				.expect(result => {
+					const subtask = result.body;
+					expect(subtask.title).toBe("Test update");
+				})
+				.expect(200, done);
+		});
+
+		it("should not change the TaskId", (done) => {
+			request(app)
+				.patch(`/subtask/${instances.subtask1.id}`)
+				.set('AuthenticationToken', authToken)
+				.send({
+					title: "Test update",
+					TaskId: instances.taskControl.id
+				})
+				.expect(result => {
+					const subtask = result.body;
+					expect(subtask.title).toBe("Test update")
+					expect(subtask.TaskId).toBe(instances.task.id);
+				})
+				.expect(200, done);
+		});
+
+		it("should not accept a non-string title", (done) => {
+			request(app)
+				.patch(`/subtask/${instances.subtask1.id}`)
+				.set('AuthenticationToken', authToken)
+				.send({
+					title: 5
+				})
+				.expect('Invalid credentials.')
+				.expect(400, done);
+		});
+
+		it("should fail correctly without a body", (done) => {
+			request(app)
+				.patch(`/subtask/${instances.subtask1.id}`)
+				.set('AuthenticationToken', authToken)
+				.expect('Invalid credentials.')
+				.expect(400, done);
+		});
+	});
+
+	describe("Tests the 'subtask_delete' controller", () => {
+		// We can limit to testing the absence of AuthenticationToken since every fail based on authentication is managed the same way
+		it("should fail because of the authentication", (done) => {
+			request(app)
+				.delete(`/subtask/${instances.subtask1.id}`)
+				.expect('Authentication required. Set `AuthenticationToken` header with the authentication token.')
+				.expect(403, done);
+		});
+
+		it("should not find the subtask", (done) => {
+			request(app)
+				.delete('/subtask/wrongid')
+				.set('AuthenticationToken', authToken)
+				.expect('No subtask found.')
+				.expect(404, done);
+		});
+
+		it("should delete the task", (done) => {
+			request(app)
+				.delete(`/subtask/${instances.subtask1.id}`)
+				.set('AuthenticationToken', authToken)
+				.expect(204)
+				.end(done);
+		})
+	})
+
+});
